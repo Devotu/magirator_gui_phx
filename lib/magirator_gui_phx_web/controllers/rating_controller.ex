968fc0a8f40ab0c038674ace114914a9a01f30a7
DevotuOne <devotu.developer@gmail.com>
2019-03-14 23:24:48 +0100

    Rating - Basic info and styling

--- lib/magirator_gui_phx_web/controllers/rating_controller.ex
+++ lib/magirator_gui_phx_web/controllers/rating_controller.ex
@@ -1,7 +1,5 @@
 defmodule MagiratorGuiPhxWeb.RatingController do
   use MagiratorGuiPhxWeb, :controller
-  alias MagiratorStore.Structs.Deck
-  alias MagiratorStore.Helpers
 
   def index(conn, _params) do
 
@@ -11,7 +9,13 @@ defmodule MagiratorGuiPhxWeb.RatingController do
       decks
       |> Enum.map( fn(deck) -> {deck.name, MagiratorStore.list_results_by_deck(deck.id)} end)
       |> Enum.map( fn({deck_name, {:ok, results}}) -> {deck_name, results} end)
-      |> Enum.map( fn({deck_name, results}) -> %{deck_name: deck_name, points: MagiratorCalculator.calculate_points(results)} end)
+      |> Enum.map( fn({deck_name, results}) -> %{
+        deck_name: deck_name, 
+        games: length(results),
+        pdiff: MagiratorCalculator.calculate_pdiff(results),
+        winrate: MagiratorCalculator.calculate_winrate(results)
+        } end)
+      |> Enum.sort(&(&1.winrate >= &2.winrate))
 
     render conn, "list.html", ratings: ratings
   end
--- lib/magirator_gui_phx_web/views/rating_view.ex
+++ lib/magirator_gui_phx_web/views/rating_view.ex
@@ -1,3 +1,10 @@
 defmodule MagiratorGuiPhxWeb.RatingView do
   use MagiratorGuiPhxWeb, :view
+
+  def red_ink(value) when value < 0 do 
+    "negative"
+  end 
+  def red_ink(_) do 
+    "positive"
+  end
 end
