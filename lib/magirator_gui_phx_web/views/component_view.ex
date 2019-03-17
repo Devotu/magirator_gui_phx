6f3ddad237c92273e3e40224b1ecc25689c22c2c
DevotuOne <devotu.developer@gmail.com>
2019-03-17 19:37:03 +0100

    Component - Player and Deck select as components

---
+++ lib/magirator_gui_phx_web/views/component_view.ex
@@ -0,0 +1,22 @@
+defmodule MagiratorGuiPhxWeb.ComponentView do
+  use MagiratorGuiPhxWeb, :view
+
+  alias MagiratorStore
+
+  def select_player(form, tag) do
+    {:ok, players} = MagiratorStore.list_players
+
+    selectable_players = Enum.map(players, fn p -> [key: p.name, value: p.id] end)
+
+    select(form, tag, selectable_players)
+  end
+
+
+  def select_deck(form, tag) do
+    {:ok, decks} = MagiratorStore.list_decks
+
+    selectable_decks = Enum.map(decks, fn d -> [key: d.name, value: d.id] end)
+
+    select(form, tag, selectable_decks)
+  end
+end
--- lib/magirator_gui_phx_web/views/deck_view.ex
+++ lib/magirator_gui_phx_web/views/deck_view.ex
@@ -1,13 +1,5 @@
 defmodule MagiratorGuiPhxWeb.DeckView do
   use MagiratorGuiPhxWeb, :view
 
-  alias MagiratorStore
-
-  def player_select(form, tag) do
-    {:ok, players} = MagiratorStore.list_players
-
-    selectable_players = Enum.map(players, fn p -> [key: p.name, value: p.id] end)
-
-    select(form, tag, selectable_players)
-  end
+  alias MagiratorGuiPhxWeb.ComponentView
 end
