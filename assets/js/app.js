d6d0899b8d86624b108cd3b07e81903816be2efc
DevotuOne <devotu.developer@gmail.com>
2019-06-11 00:07:21 +0200

    Ratings - Sort dynamic target

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,22 @@ import { sortFunctions } from "./sort.js"
 Array.from(document.getElementsByClassName("sortButton")).forEach(e => 
   e.addEventListener('click', () => 
     {
-      sortFunctions.sortTableByColumn('statTable', e.dataset.sortColumn); 
+      sortFunctions.sortTableByColumn(findSortParent(e), e.dataset.sortColumn); 
     }, false)
   );
+
+function findSortParent(element) {
+
+  if (element.parentElement === null || typeof element.parentElement === "undefined") 
+  {
+    console.log("No parent element containing marker");
+    return "";
+  }
+
+  if (typeof element.parentElement.dataset.sortTarget === "undefined") 
+  {
+    return findSortParent(element.parentElement);
+  }
+
+  return element.parentElement.dataset.sortTarget;
+}
--- assets/js/sort.js
+++ assets/js/sort.js
@@ -1,52 +1,58 @@
 var sortFunctions = (function () 
 {
   function sortTableByColumn(target, column) { 
-    console.log('sorting ' + target + ' on column ' + column) 
 
     let table = document.getElementById(target);
     let sorting = true;
-    
-    while (sorting) {
-      sorting = false;
-      let rows = table.rows;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
 
-      // Start by saying there should be no switching:
-      let shouldSwitch = false;
-
-      let i = 0;
-      for (i = 1; i < (rows.length - 1); i++) {
-        // /* Get the two elements you want to compare,
-        // one from current row and one from the next: */
-        let x = rows[i].cells[column];
-        let y = rows[i + 1].cells[column];
-
-        // // Check if the two rows should switch place:
-        if (isNumeric(x.innerHTML)) 
-        {
-          if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
-        } 
-        else 
-        {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
+    if (table != null && typeof table !== "undefined") 
+    {
+      while (sorting) {
+        sorting = false;
+        let rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+  
+        // Start by saying there should be no switching:
+        let shouldSwitch = false;
+  
+        let i = 0;
+        for (i = 1; i < (rows.length - 1); i++) {
+          // /* Get the two elements you want to compare,
+          // one from current row and one from the next: */
+          let x = rows[i].cells[column];
+          let y = rows[i + 1].cells[column];
+  
+          // // Check if the two rows should switch place:
+          if (isNumeric(x.innerHTML)) 
+          {
+            if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } 
+          else 
+          {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
           }
         }
+  
+        if (shouldSwitch) {
+          /*If a switch has been marked, make the switch
+          and mark that a switch has been done:*/
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          sorting = true;
+        }
       }
-
-      if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        sorting = true;
-      }
+    }
+    else
+    {
+      console.log('No such element ' + target) 
     }
   }
 
