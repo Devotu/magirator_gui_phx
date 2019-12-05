381d71b9236e65c7eca495e4839e7acf0645aebc
DevotuOne <devotu.developer@gmail.com>
2019-12-05 20:19:50 +0100

    Ratings - Adopt new calculator names

--- lib/magirator_gui_phx_web/helpers/collector.ex
+++ lib/magirator_gui_phx_web/helpers/collector.ex
@@ -2,20 +2,20 @@ defmodule MagiratorGuiPhx.Helpers.Collector do
 
   def collect_game_statistics(results) do
     %{
-      games: MagiratorCalculator.count_aggregate_games(results), 
-      wins: MagiratorCalculator.count_aggregate_wins(results), 
-      draws: MagiratorCalculator.count_aggregate_draws(results),
-      losses: MagiratorCalculator.count_aggregate_losses(results),  
-      winrate: MagiratorCalculator.calculate_aggregate_winrate(results)
+      games: MagiratorCalculator.count_summary_list_games(results), 
+      wins: MagiratorCalculator.count_summary_list_wins(results), 
+      draws: MagiratorCalculator.count_summary_list_draws(results),
+      losses: MagiratorCalculator.count_summary_list_losses(results),  
+      winrate: MagiratorCalculator.calculate_summary_list_winrate(results)
     }
   end
 
   def collect_rating_data(results) do    
     %{
-      pdiff: MagiratorCalculator.calculate_aggregate_pdiff(results),
-      pdiff3: MagiratorCalculator.calculate_aggregate_pdiff_cap(results, 3),
-      pdist2positive: MagiratorCalculator.calculate_aggregate_pdist_positive(results, 2),
-      pdist2: MagiratorCalculator.calculate_aggregate_pdist(results, 2)
+      pdiff: MagiratorCalculator.calculate_summary_list_pdiff(results),
+      pdiff3: MagiratorCalculator.calculate_summary_list_pdiff_cap(results, 3),
+      pdist2positive: MagiratorCalculator.calculate_summary_list_pdist_positive(results, 2),
+      pdist2: MagiratorCalculator.calculate_summary_list_pdist(results, 2)
     }
   end
 end
